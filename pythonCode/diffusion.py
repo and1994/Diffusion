7d7d45044c134ca3d4f3d311c8229344504155e5
Andrea Marcheggiani <zx825273@live.reading.ac.uk>
2017-10-17 15:52:24 +0100

    Saving changes to diffusion

--- pythonCode/diffusion.py
+++ pythonCode/diffusion.py
@@ -14,4 +14,32 @@ execfile("diagnostics.py")
 execfile("initialConditions.py")
 
 def main():
-    # Insert the rest of the code for main here
+    """
+    Diffuse a squareWave between squareWaveMin and squareWaveMax on a domain
+    between x = xmin and x = xmax split over nx spatial steps with diffusion
+    coefficient K, time step dt for nt time steps.
+    """
+    # parameters
+    xmin = 0.
+    xmax = 1.
+    nx = 21
+    nt = 20
+    dt = 0.1
+    K = 2e-3
+    squareWaveMin = 0.4
+    squareWaveMax = 0.6
+    
+    # derived parameters
+    dx = (xmax - xmin)/(nx-1)
+    d = K*dt/dx**2   # non-dimensional diffusion coefficient
+    print("non-dimensional diffusion coefficient = ", d)
+    print("dx = ", dx, " dt = ", dt, " nt = ", nt)
+    print("end time = ", nt*dt)
+    
+    # spatial points for plotting and for defining initial conditions
+    x = np.zeros(nx)
+    for j in range(nx):
+        x[j] = xmin + j*dx
+    print('x = ', x)
+    
+    # initial conditions
