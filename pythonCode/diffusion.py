49ed15e6408b061c7c9d0a33b49da237eae0a942
and1994 <and.199four@gmail.com>
2017-10-18 19:30:56 +0100

    Improving
    Parameters inside main arguments, so that is callable from the command line. Added plot of errors.

--- pythonCode/diffusion.py
+++ pythonCode/diffusion.py
@@ -13,21 +13,14 @@ runfile("diffusionSchemes.py")
 runfile("diagnostics.py")
 runfile("initialConditions.py")
 
-def main():
+def main(xmin = 0., xmax = 1., nx = 41, nt = 40, dt = 0.1, K = 1e-3, \
+         squareWaveMin = 0.4, squareWaveMax = 0.6, name_fig='figure'):
     """
     Diffuse a squareWave between squareWaveMin and squareWaveMax on a domain
     between x = xmin and x = xmax split over nx spatial steps with diffusion
     coefficient K, time step dt for nt time steps.
     """
-    # parameters
-    xmin = 0.
-    xmax = 1.
-    nx = 21
-    nt = 20
-    dt = 0.1
-    K = 2e-3
-    squareWaveMin = 0.4
-    squareWaveMax = 0.6
+    # default parameters set in the function arguments
     
     # derived parameters
     dx = (xmax - xmin)/(nx-1)
@@ -57,7 +50,7 @@ def main():
     print("BTCS L2 error norm = ", L2ErrorNorm(phiBTCS, phiAnalytic))
     
     # plot the solutions
-    font = {'size' : 20}
+    font = {'size' : 10}
     plt.rc('font', **font)
     plt.figure(1)
     plt.clf()
@@ -71,10 +64,18 @@ def main():
     plt.ylim([0,1])
     plt.legend(bbox_to_anchor=(1.1, 1))
     plt.xlabel('$x$')
-    plt.savefig('plots/FTCS_BTCS.pdf')
+    plt.savefig('Plots/'+name_fig+'.pdf')
     
-main()    
+    # plot the errors
+    plt.figure(2)
+    plt.clf()
+    plt.ion()
+    plt.plot(x, phiFTCS - phiAnalytic, label='Error FTCS', color='blue')
+    plt.plot(x, phiBTCS - phiAnalytic, label='Error BTCS', color='red')
+    plt.axhline(0, linestyle=':', color='black')
+    plt.ylim([-0.05,0.05])
+    plt.legend(bbox_to_anchor=(1.1, 1))
+    plt.xlabel('$x$')
+    plt.savefig('Plots/'+name_fig+'_errors.pdf')
     
-    
-    
-    
+main(name_fig='attempt')
