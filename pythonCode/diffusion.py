fcbd7a4ae3412a0a21a568b658039c9bc8329601
and1994 <and.199four@gmail.com>
2017-10-25 19:38:18 +0100

    Changes to diffusion
    Changes to main in order to be used in nrms_error_graph so that it does not show values calculated for each iteration. This last function builds the L2 error graph by plotting the L2 error Norms on a log-log plot.

--- pythonCode/diffusion.py
+++ pythonCode/diffusion.py
@@ -13,7 +13,7 @@ runfile("diffusionSchemes.py")
 runfile("diagnostics.py")
 runfile("initialConditions.py")
 
-def main(xmin = 0., xmax = 1., nx = 41, nt = 40, dt = 0.1, K = 1e-3, \
+def main(xmin = 0., xmax = 1., nx = 41, d = 0.16, K = 1e-3, \
          squareWaveMin = 0.4, squareWaveMax = 0.6, name_fig='Question3'):
     """
     Diffuse a squareWave between squareWaveMin and squareWaveMax on a domain
@@ -24,16 +24,17 @@ def main(xmin = 0., xmax = 1., nx = 41, nt = 40, dt = 0.1, K = 1e-3, \
     
     # derived parameters
     dx = (xmax - xmin)/(nx-1)
-    d = K*dt/dx**2   # non-dimensional diffusion coefficient
-    print("non-dimensional diffusion coefficient = ", d)
-    print("dx = ", dx, " dt = ", dt, " nt = ", nt)
-    print("end time = ", nt*dt)
+    dt = d*dx**2/K   # time step imposing the desired value of d
+    nt = int(4.0/dt) # imposing same duration for every run
+    #print("non-dimensional diffusion coefficient = ", d)
+    #print("dx = ", dx, " dt = ", dt, " nt = ", nt)
+    #print("end time = ", nt*dt)
     
     # spatial points for plotting and for defining initial conditions
     x = np.zeros(nx)
     for j in range(nx):
         x[j] = xmin + j*dx
-    print('x = ', x)
+    #print('x = ', x)
     
     # initial conditions
     phiOld = squareWave(x, squareWaveMin, squareWaveMax)
@@ -46,9 +47,12 @@ def main(xmin = 0., xmax = 1., nx = 41, nt = 40, dt = 0.1, K = 1e-3, \
     phiBTCS = BTCS(phiOld.copy(), d, nt)
     
     # calculate and print out error norms
-    print("FTCS L2 error norm = ", L2ErrorNorm(phiFTCS, phiAnalytic))
-    print("BTCS L2 error norm = ", L2ErrorNorm(phiBTCS, phiAnalytic))
+    L2errFTCS = L2ErrorNorm(phiFTCS, phiAnalytic)
+    L2errBTCS = L2ErrorNorm(phiBTCS, phiAnalytic)
+    #print("FTCS L2 error norm = ", L2errFTCS)
+    #print("BTCS L2 error norm = ", L2errBTCS)
     
+    """
     # plot the solutions
     font = {'size' : 10}
     plt.rc('font', **font)
@@ -86,5 +90,25 @@ def main(xmin = 0., xmax = 1., nx = 41, nt = 40, dt = 0.1, K = 1e-3, \
     plt.xlabel('$x$')
     plt.title("t = {:.1f}, d = {:.2f}".format(nt*dt, d))
     plt.savefig('Plots/' + name_fig + '(t=' + str(int(nt*dt)) + ')_errors.pdf')
+    """
     
-    
+    return dx, L2errFTCS, L2errBTCS
+
+def nrms_error_graph(N):
+    vector = np.zeros((N,3))
+    for it in range(N):
+        vector[it,:] = main(nx=21+it*50,d=0.16)
+    font = {'size' : 10}
+    plt.rc('font', **font)
+    plt.figure(3)
+    plt.clf()
+    plt.ion()
+    plt.loglog(vector[:,0].transpose(), vector[:,1].transpose(), label='FTCS',\
+               color='blue')
+    plt.loglog(vector[:,0].transpose(), vector[:,2].transpose(), label='BTCS',\
+               color='red')
+    plt.legend()
+    plt.xlabel('$\Delta x$')
+    plt.ylabel('$\ell_2$ Norm Error')
+    plt.title('Log-log plot of $\ell_2$ Norm Error')
+    plt.savefig('Plots/L2errorPlot{}.pdf'.format(int(N)))
